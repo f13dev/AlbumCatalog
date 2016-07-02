43971dcc74fc054e251cc820f3cf7e3eef125073
James Valentine <jv@f13dev.com>
2016-07-02 11:16:49 +0100

    Adding onClickListener to AddAlbumAcvtivity.class

--- app/src/main/java/com/f13dev/albumcatalog/AddAlbumActivity.java
+++ app/src/main/java/com/f13dev/albumcatalog/AddAlbumActivity.java
@@ -1,13 +1,36 @@
 package com.f13dev.albumcatalog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class AddAlbumActivity extends AppCompatActivity {
+public class AddAlbumActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_album);
+
+        Button buttonBack = (Button) findViewById(R.id.btnBack);
+
+        buttonBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent();
+
+        if (view.getId() == R.id.btnBack)
+        {
+            i = new Intent(this, AlbumCatalogMain.class);
+        }
+        else
+        {
+            i = new Intent(this, AddAlbumActivity.class);
+        }
+        startActivity(i);
+
     }
 }
